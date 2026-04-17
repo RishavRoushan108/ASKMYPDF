073a99163776d5885a24a84c3b6a08d07c799ec7
rishavroushan98 <rishavroushan98@gmail.com>
2026-04-17 07:26:53 +0530

    added middleware and auth pages

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // 1. Define PUBLIC routes (anyone can see these)
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware(async (auth, req) => {
   // 2. Protect everything else
